b6d9480d5bb3b4ec8e9213dd343e25983c4bc4e6
Fauzaan <46840205+FauzaanSheikh@users.noreply.github.com>
2026-01-07 05:49:14 -0500

    update config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Uncomment and update if deploying to a subpath (e.g., username.github.io/repo-name)
-  // basePath: '/Portfolio',
-  // assetPrefix: '/Portfolio/',
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
 };
 
 export default nextConfig;
